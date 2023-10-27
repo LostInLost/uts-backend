977cb3de156d94bdaf4ae6ccb35580edad609c91
Affansyah Hanan W <afanhanan10@gmail.com>
2023-10-27 18:53:48 +0700

    Deploy Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/css/app.css'])
     <title>Lastation</title>
 
 </head>
